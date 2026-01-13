1f2a906a12414e7aa44b713ee2e55c7ec60c1e32
Joud Rashed <joudrashed2808@gmail.com>
2026-01-13 20:46:31 +0200

    Refactor edge detection function for grayscale images

--- image_utils.py
+++ image_utils.py
@@ -2,29 +2,28 @@ from PIL import Image
 import numpy as np
 from scipy.signal import convolve2d
 
-
 def load_image(path):
-    img = Image.open(path).convert("L")
+    img = Image.open(path)
     img = np.array(img, dtype=np.float32)
     return img
-    
-def edge_detection(image):
 
-    sobel_x = np.array([
+def edge_detection(img):
+    gray = img.mean(axis=2)
+
+    kernelY = np.array([
+        [1, 2, 1],
+        [0, 0, 0],
+        [-1, -2, -1]
+    ])
+
+    kernelX = np.array([
         [-1, 0, 1],
         [-2, 0, 2],
         [-1, 0, 1]
     ])
 
-    sobel_y = np.array([
-        [-1, -2, -1],
-        [ 0,  0,  0],
-        [ 1,  2,  1]
-    ])
+    edgeX = convolve2d(gray, kernelX, mode="same", boundary="fill", fillvalue=0)
+    edgeY = convolve2d(gray, kernelY, mode="same", boundary="fill", fillvalue=0)
 
-    gx = convolve2d(image, sobel_x, mode="same", boundary="symm")
-    gy = convolve2d(image, sobel_y, mode="same", boundary="symm")
-
-    edges = np.sqrt(gx**2 + gy**2)
-
-    return edges
+    edgeMAG = np.sqrt(edgeX**2 + edgeY**2)
+    return edgeMAG
