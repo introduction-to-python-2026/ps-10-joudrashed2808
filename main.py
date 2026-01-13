d6a08903252b3c6035a4dd3328825c5fa879fca7
Joud Rashed <joudrashed2808@gmail.com>
2026-01-13 20:46:54 +0200

    Implement image edge detection and saving
    Load an image, apply median filtering, and perform edge detection before saving the result.

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from image_utils import load_image, edge_detection
+from skimage.filters import median
+from skimage.morphology import ball
+from PIL import Image
+import numpy as np
+
+img = load_image("image.jpg")
+
+clean_img = median(img, ball(3))
+
+edgeMAG = edge_detection(clean_img)
+
+threshold = edgeMAG.mean()
+edge_binary = edgeMAG > threshold
+
+edge_binary = (edge_binary * 255).astype(np.uint8)
+Image.fromarray(edge_binary).save("my_edges.png")
 
